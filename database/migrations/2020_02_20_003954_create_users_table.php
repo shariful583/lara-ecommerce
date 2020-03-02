dc3a936dc95caadd20fd61f9460c279cc91a6ea1
shariful583 <shariful.cse583@gmail.com>
2020-03-02 13:30:45 +0600

    Database file updated

--- database/migrations/2020_02_20_003954_create_users_table.php
+++ database/migrations/2020_02_20_003954_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name', 128);
             $table->string('email', 50)->unique();
-            $table->string('phone', 20)->unique();
+            $table->string('phone', 32)->unique();
             $table->string('password', 128);
             $table->bigInteger('reward_points')->default(0);
             $table->date('email_verified_at')->nullable();
