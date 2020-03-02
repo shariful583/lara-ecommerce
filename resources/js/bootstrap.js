b29fcea4a9e4149d062a43b6116cb0c8c7114da3
shariful583 <shariful.cse583@gmail.com>
2020-03-02 13:32:45 +0600

    style and js file added

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,15 +6,31 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// window.axios = require('axios');
+//
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
+
+
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+    require('bootstrap');
+
+} catch (e) {
+
+}
+
+
+
+
+
 
 // import Echo from 'laravel-echo';
 
