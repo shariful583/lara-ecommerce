ca8deb2888dd1ad6261972d4893609fb970793b9
shariful583 <shariful.cse583@gmail.com>
2020-03-02 14:04:20 +0600

    Some file change

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,37 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::group(['namespace' => 'Frontend'],function (){
+    Route::get('/', 'HomeController@showHomePage')->name('frontend.home');
+    Route::get('/product/{slug}', 'ProductController@showDetails')->name('frontend.details');
+    Route::get('/cart', 'CartController@showCart')->name('cart.show');
+    Route::post('/cart', 'CartController@addToCart')->name('cart.add');
+    Route::post('/cart/remove', 'CartController@removeFromCart')->name('cart.remove');
+    Route::get('/cart/clear', 'CartController@clearFromCart')->name('cart.clear');
+    Route::get('/checkout', 'CartController@checkout')->name('cart.checkout');
+
+
+
+    Route::get('/login', 'AuthController@showLogin')->name('user.login');
+    Route::post('/login', 'AuthController@processLogin');
+
+
+    Route::get('/signup', 'AuthController@showSignup')->name('user.signup');
+    Route::post('/signup', 'AuthController@processSignup');
+
+
+    Route::get('/activate/{token}', 'AuthController@activate')->name('activate');
+
+    Route::group(['middleware' => 'auth'], function (){
+        Route::post('/order', 'CartController@processOrder')->name('order');
+        Route::get('/order/{id}', 'CartController@orderDetails')->name('order.details');
+
+
+        Route::get('/profile', 'AuthController@profile')->name('user.profile');
+        Route::get('/logout', 'AuthController@logout')->name('user.logout');
+    });
 });
+
+
