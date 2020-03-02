53822ed03f8d03b65a72d7d2a31e13ddda3063a1
shariful583 <shariful.cse583@gmail.com>
2020-03-02 13:34:36 +0600

    Notification added

---
+++ app/Notifications/RegistrationEmailNotification.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class RegistrationEmailNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+    private $user;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Welcome '.$this->user->name.'to '.config(app.name))
+                    ->line('Please click to the following link')
+                    ->action('Click here',route('activate',$this->user->email_verification_token))
+                    ->line('Thank you for using our application!');
+    }
+
+}
